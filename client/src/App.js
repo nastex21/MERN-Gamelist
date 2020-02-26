d699e4f8594adfd336a6f063c7a21c353aeddb01
Tony <nasax2000@gmail.com>
2020-02-25 21:27:51 -0600

    Commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,21 @@
 import React, { Component } from "react";
 import axios from "axios";
-import GenerateTable from './Components/generateTable';
+import GenerateTable from "./Components/generateTable";
 import Table from "react-bootstrap/Table";
 import Pagination from "react-bootstrap/Pagination";
 
 export default class App extends Component {
   state = {
-    value: '',
+    value: "",
     games: []
   };
 
-  handleChange = (event) => {
+  handleChange = event => {
     console.log(event.target.value);
     this.setState({ value: event.target.value });
-  }
+  };
 
-  handleSubmit = (event) => {
+  handleSubmit = event => {
     event.preventDefault();
     var test = "http://localhost:5555/";
     var steamID = this.state.value;
@@ -36,18 +36,29 @@ export default class App extends Component {
       <div>
         <div className="button">
           <form onSubmit={this.handleSubmit}>
-            <p>If you know your Steam ID and your profile is public, please use your Steam ID otherwise please sign in with your Steam account.</p>
-            <input type="text" value={this.state.value} onChange={this.handleChange} />
+            <p>
+              If you know your Steam ID and your profile is public, please use
+              your Steam ID otherwise please sign in with your Steam account.
+            </p>
+            <input
+              type="text"
+              value={this.state.value}
+              onChange={this.handleChange}
+            />
             <input type="submit" value="Get Steam games" />
           </form>
           <div className="steamLogIn">
-            <img src="https://steamcdn-a.akamaihd.net/steamcommunity/public/images/steamworks_docs/english/sits_large_border.png" />
+            <a href="http://localhost:5555/auth/steam">
+              <img src="https://steamcdn-a.akamaihd.net/steamcommunity/public/images/steamworks_docs/english/sits_large_border.png" />
+            </a>
           </div>
           {this.state.games.length === 0 ? null : (
             <p>You have {this.state.games.length} games</p>
           )}
         </div>
-        {this.state.games.length === 0 ? null : <GenerateTable gamelist={this.state.games} />}
+        {this.state.games.length === 0 ? null : (
+          <GenerateTable gamelist={this.state.games} />
+        )}
       </div>
     );
   }
--- index.js
+++ index.js
@@ -1,11 +1,46 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-const axios = require('axios');
-const bodyParser = require('body-parser');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const session = require("express-session");
+const passport = require("passport");
+const SteamStrategy = require("passport-steam").Strategy;
+const path = require("path");
+const axios = require("axios");
+const bodyParser = require("body-parser");
 const app = express();
 
-console.log("yes it's working");
+passport.serializeUser((user, done) => {
+  done(null, user._json);
+});
+
+passport.deserializeUser((obj, done) => {
+  done(null, obj);
+});
+
+passport.use(
+  new SteamStrategy(
+    {
+      returnURL: "http://localhost:5555/auth/steam/return",
+      realm: "http://localhost:5555",
+      apiKey: process.env.KEY
+    },
+    (identifier, profile, done) => {
+      return done(null, profile);
+    }
+  )
+);
+
+app.use(
+  session({
+    secret: process.env.SECRET,
+    name: "U_SESSION",
+    resave: true,
+    saveUninitialized: true
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use(cors());
 
@@ -15,25 +50,37 @@ app.use(bodyParser.json());
 //support parsing of application/x-www-form-urlencoded post data
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.post('/', function (req, res) {
-    console.log("WORKING?!");
-    console.log(req.body);
-    const userID = req.body.value;
-    console.log(userID);
-    var httpVar = `http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=${process.env.KEY}&steamid=${userID}&include_appinfo=true&format=json`;
-    console.log(httpVar);
-     try {
-        console.log("try axios working");
-        axios.get(httpVar)
-            .then(data => res.send(data.data.response))
-            .catch(err => res.send(err));
-    }
-    catch (err) {
-        console.error("GG", err);
-    } 
-
+app.get('/', (req,res) => {
+    console.log('hi');
 })
 
+app.get(
+  /^\/auth\/steam(\/return)?$/,
+  passport.authenticate("steam", { failureRedirect: "/" }),
+  (req, res) => {
+    console.log("REDIRECTED")
+    res.redirect("/");
+  }
+);
+
+app.get("/logout", (req, res) => {
+  req.logout();
+  res.redirect("/");
+});
+
+app.post("/", function(req, res) {
+  const userID = req.body.value;
+  var httpVar = `http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=${process.env.KEY}&steamid=${userID}&include_appinfo=true&format=json`;
+  try {
+    console.log("try axios working");
+    axios
+      .get(httpVar)
+      .then(data => res.send(data.data.response))
+      .catch(err => res.send(err));
+  } catch (err) {
+    console.error("GG", err);
+  }
+});
 
 // Routes
 //app.use('/api/dashboard', require('./routes/api/dashboard') );
