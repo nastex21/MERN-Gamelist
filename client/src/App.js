41f8000dda04b760ac7207e14345ca67055e9b0d
nastex21 <nasax2000@gmail.com>
2020-02-25 18:24:55 -0600

    Commit

--- client/src/App.js
+++ client/src/App.js
@@ -36,10 +36,13 @@ export default class App extends Component {
       <div>
         <div className="button">
           <form onSubmit={this.handleSubmit}>
-            <p>If you know your Steam ID and your profile is public, please use your Steam ID.</p>
+            <p>If you know your Steam ID and your profile is public, please use your Steam ID otherwise please sign in with your Steam account.</p>
             <input type="text" value={this.state.value} onChange={this.handleChange} />
             <input type="submit" value="Get Steam games" />
           </form>
+          <div className="steamLogIn">
+            <img src="https://steamcdn-a.akamaihd.net/steamcommunity/public/images/steamworks_docs/english/sits_large_border.png" />
+          </div>
           {this.state.games.length === 0 ? null : (
             <p>You have {this.state.games.length} games</p>
           )}
