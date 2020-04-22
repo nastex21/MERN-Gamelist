e439db36459999ed869896b6b10079241676a678
nastex21 <nasax2000@gmail.com>
2020-04-22 01:22:04 -0500

    Commit.

--- client/src/Components/AuthPages/Login.js
+++ client/src/Components/AuthPages/Login.js
@@ -5,11 +5,15 @@ import setAuthToken from "../../utils/setAuthToken";
 import jwt_decode from "jwt-decode";
 import axios from 'axios';
 import "../css/Forms.css";
+import Alert from 'react-bootstrap/Alert';
 
 export default function LoginPage() {
   const [name, setName] = useState("");
   const [password, setPassword] = useState("");
   const [redirectPage, setRedirect] = useState(false);
+  const [registrationSuccess, setSuccess] = useState(false);
+  const [loginFail, setFail] = useState(false);
+  const [show, setShow] = useState(true);
 
   localStorage.removeItem("guest");
   localStorage.removeItem("stored-steamgamedata");
@@ -47,30 +51,56 @@ export default function LoginPage() {
         localStorage.removeItem('guest');
         // Set token to Auth header
         setAuthToken(token);
+        setFail(false);
         // Decode token to get user data
         const decoded = jwt_decode(token);
 
         setRedirect(true);
       })
-      .catch(err =>
-        console.log(err)
-      );
-  };
+      .catch(err => {
+        setFail(true);
+      })
+  }
 
-  return (
-    <>
-      {redirectPage ? (
-        <Redirect to="/dashboard" />
-      ) : (
+  const successMsg = () => {
+    return (
+      <div class="alert alert-success" role="alert">
+        Sucessfully registered! Please log in.
+      </div>
+    )
+  }
+
+  const failMsg = () => {
+    if (show) {
+      return (
+        <Alert variant="danger" onClose={() => setShow(false)} dismissible>
+          <Alert.Heading>Oh snap! You got an error!</Alert.Heading>
+          <p>
+            Change this and that and try again. Duis mollis, est non commodo
+            luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit.
+            Cras mattis consectetur purus sit amet fermentum.
+      </p>
+        </Alert>
+      )
+    }
+  }
+
+return (
+  <>
+    {redirectPage ? (
+      <Redirect to="/dashboard" />
+    ) : (
         <div className="loginPage">
+          {registrationSuccess ? successMsg() : null}
+          {loginFail ? failMsg() : null}
           <LoginForm
             handleSubmit={handleSubmit}
             name={name}
             onChange={onChange}
             password={password}
           />
-          </div>
-        )}
-    </>
-  );
+        </div>
+      )}
+  </>
+);
 }
