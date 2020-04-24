85ed6a57ece026b67728c886e4d1b268bfcfc6c7
nastex21 <nasax2000@gmail.com>
2020-04-24 00:52:02 -0500

    Fix the logic

--- client/src/Components/AuthPages/Forms/RegisterForm.js
+++ client/src/Components/AuthPages/Forms/RegisterForm.js
@@ -1,7 +1,28 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import "../../css/Forms.css";
 
-export default function RegisterForm({ handleSubmit, onChange, password, password2, name  }) {
+const useMountEffect = (fun) => useEffect(fun, []);
+
+// Gneral Focus Hook
+const UseFocus = () => {
+  const htmlElRef = useRef(null)
+  const setFocus = () => { htmlElRef.current && htmlElRef.current.focus() }
+
+  return [htmlElRef, setFocus]
+}
+
+const isBoolean = (param) => typeof (param) === "boolean"
+
+export default function RegisterForm({ handleSubmit, onChange, password, password2, name }) {
+  const [input1Val, setInput1Val] = useState("")
+  const [input1Ref, setInput1Focus] = UseFocus()
+
+  const [input2Val, setInput2Val] = useState("")
+  const [input2Ref, setInput2Focus] = UseFocus()
+
+  const [completeBtnRef, setCompleteFocus] = UseFocus()
+
+  useMountEffect(setInput1Focus)
   return (
     <div className="formsGroup">
       <form noValidate onSubmit={handleSubmit}>
@@ -11,10 +32,15 @@ export default function RegisterForm({ handleSubmit, onChange, password, passwor
           <label>Username or Email address</label>
           <input
             className="form-control"
-            onChange={onChange}
-            value={name}
+            onChange={(e)=>{
+              const val = e.target.value 
+              setInput1Val(val)
+              if (val.length===1) setInput2Focus()
+            }}
+            value={input1Val}
             id="name"
             type="text"
+            ref={input1Ref}
             placeholder="Enter username or email"
           />
         </div>
@@ -24,8 +50,13 @@ export default function RegisterForm({ handleSubmit, onChange, password, passwor
           <input
             type="password"
             className="form-control"
-            onChange={onChange}
-            value={password}
+            onChange={(e)=>{
+              const val = e.target.value
+              setInput2Val(val)
+              if (val.length===2) setCompleteFocus()
+            }}
+            ref={input2Ref}
+            value={input2Val}
             id="password"
             placeholder="Enter password"
           />
@@ -47,6 +78,7 @@ export default function RegisterForm({ handleSubmit, onChange, password, passwor
           type="submit"
           className="btn btn-primary btn-block"
           value="Submit"
+          ref={completeBtnRef}
         />
         <p className="forgot-password text-right">
           Already registered <a href="/login">sign in?</a>
