bb9027b26d81bb1773b1d6abb606a214f957cbdf
Tony <nasax2000@gmail.com>
2020-04-25 02:49:17 -0500

    Styling the login component

--- client/src/Components/AuthPages/Forms/LoginForm.js
+++ client/src/Components/AuthPages/Forms/LoginForm.js
@@ -13,7 +13,7 @@ const UseFocus = () => {
   return [htmlElRef, setFocus];
 };
 
-export default function LoginForm({ handleSubmit, onChange, password, name}) {
+export default function LoginForm({ handleSubmit, onChange, password, name }) {
   const [type, setType] = useState("password");
   const [input1Ref, setInput1Focus] = UseFocus();
   const [input2Ref, setInput2Focus] = UseFocus();
@@ -25,50 +25,83 @@ export default function LoginForm({ handleSubmit, onChange, password, name}) {
   };
 
   useMountEffect(setInput1Focus);
-  
+
   return (
     <div className="formsGroup">
-      <form onSubmit={handleSubmit}>
-        <h3>Log In</h3>
+      <div className="container-login100">
+        <div className="wrap-login100 p-l-85 p-r-85 p-t-55 p-b-55">
+          <form
+            className="login100-form validate-form flex-sb flex-w"
+            onSubmit={handleSubmit}
+          >
+            <span className="login100-form-title p-b-32">Log In</span>
 
-        <div className="form-group">
-          <label>Username</label>
-          <input
-            type="text"
-            className="form-control"
-            onChange={onChange}
-            value={name}
-            id="name"
-            ref={input1Ref}
-          />
+            <span className="txt1 p-b-11">Username</span>
+            <div className="wrap-input100 m-b-36">
+              <input
+                type="text"
+                className="input100"
+                onChange={onChange}
+                value={name}
+                id="name"
+                ref={input1Ref}
+                name="username"
+              />
+              <span className="focus-input100"></span>
+            </div>
+
+            <span className="txt1 p-b-11">Password</span>
+            <div className="wrap-input100 m-b-12">
+              <span class="btn-show-pass">
+                <i class="fa fa-eye"></i>
+              </span>
+              <input
+                type={type}
+                className="input100"
+                onChange={onChange}
+                value={password}
+                ref={input2Ref}
+                id="password"
+                name="pass"
+              />
+              <span className="focus-input100"></span>
+            </div>
+
+            <div className="flex-sb-m w-full p-b-48">
+              {/* <input
+                type="checkbox"
+                className="form-check-input"
+                id="exampleCheck1"
+                onClick={showHide}
+              />
+              <label className="form-check-label" htmlFor="exampleCheck1">
+                Show Password
+              </label>
+               */}
+              <div className="contact100-form-checkbox">
+                <input
+                  className="input-checkbox100"
+                  id="ckb1"
+                  type="checkbox"
+                  name="remember-me"
+                />
+                <label className="label-checkbox100" for="ckb1">
+                  Remember me
+                </label>
+              </div>
+            </div>
+
+            {/* <input
+              type="submit"
+              className="btn btn-primary btn-block"
+              ref={completeBtnRef}
+            /> */}
+            <div className="container-login100-form-btn">
+              <button className="login100-form-btn">Login</button>
+            </div>
+          </form>
         </div>
-
-        <div className="form-group">
-          <label>Password</label>
-          <input
-            type={type}
-            className="form-control"
-            onChange={onChange}
-            value={password}
-            ref={input2Ref}
-            id="password"
-          />
-        </div>
-
-        <div className="form-check">
-          <input
-            type="checkbox"
-            className="form-check-input"
-            id="exampleCheck1"
-            onClick={showHide}
-          />
-          <label className="form-check-label" htmlFor="exampleCheck1">
-            Show Password
-          </label>
-        </div>
-
-        <input type="submit" className="btn btn-primary btn-block"  ref={completeBtnRef} />
-      </form>
+      </div>
     </div>
   );
 }
