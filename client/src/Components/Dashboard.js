915c94c342fcd9238eae251cd46ab706f7ceaa67
Tony <nasax2000@gmail.com>
2020-05-11 17:51:13 -0500

    Mobile: Made steambox div responsive

--- client/src/Components/Dashboard.js
+++ client/src/Components/Dashboard.js
@@ -2,6 +2,9 @@ import React from "react";
 import ManuallyAdded from "./Pull-Gamelists/ManualEntries/ManuallyAdded";
 import SteamForm from "./Pull-Gamelists/SteamList/SteamForm";
 import GenerateTable from "./GenerateTable/GenerateTable";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 export default function Dashboard({
   manualData,
@@ -12,43 +15,67 @@ export default function Dashboard({
   handleSubmit,
   handleClick,
   games,
-  games2, 
+  games2,
   updateSteamGames,
   deletedGamesRender,
-  successAddMsg
-})
-
-
-{
+  successAddMsg,
+}) {
   console.log(successAddMsg);
   return (
-    <div className="dashboard">
-      <div className="manualBox">
-        <ManuallyAdded uploadData={manualData} />
+    <div className="buttonBox">
+      <div className="steamBox">
+        <Container fluid="true">
+          <Row className="align-items-center">
+            {!steamId ? (
+              <Col xs={5}>
+                <SteamForm
+                  value={value}
+                  onChange={handleChange}
+                  submit={handleSubmit}
+                />
+              </Col>
+            ) : null}
+            <Col md="auto" xs={1}>
+              <div className="vertLine"></div>
+            </Col>
+            {!steamId ? (
+              <Col xs={5}>
+                <div className="steamLogIn text-center">
+                  <a onClick={handleClick}>
+                    <img className="steamIMG" src="https://steamcommunity-a.akamaihd.net/public/images/signinthroughsteam/sits_01.png" />
+                  </a>
+                </div>
+              </Col>
+            ) : null}
+            {steamId ? (
+              <input
+                type="button"
+                value="Update Steam Games"
+                onClick={updateSteamGames}
+              />
+            ) : null}
+          </Row>
+        </Container>
       </div>
-      <div className="buttonBox">
-        {!steamId ? (
-          <SteamForm
-            value={value}
-            onChange={handleChange}
-            submit={handleSubmit}
-          />
-        ) : null}
-        <hr />
-        {!steamId ? (
-          <div className="steamLogIn">
-            <a onClick={handleClick}>
-              <img src="https://steamcommunity-a.akamaihd.net/public/images/signinthroughsteam/sits_01.png" />
-            </a>
-          </div>
-        ) : null}
-        {steamId ? <input type="button" value="Update Steam Games" onClick={updateSteamGames}/> : null}
+
+      <div className="dashboard">
+        <div className="manualBox">
+          <ManuallyAdded uploadData={manualData} />
+        </div>
         {games.length === 0 ? null : (
           <p>You have {games.length + games2.length} games</p>
         )}
       </div>
       <hr />
-        {games.length === 0 && games2.length === 0 ? null : <GenerateTable gamelist={games} gameslist2={games2} userId={userId} deletedGamesRender={deletedGamesRender} successAddMsg={successAddMsg}/>} 
+      {games.length === 0 && games2.length === 0 ? null : (
+        <GenerateTable
+          gamelist={games}
+          gameslist2={games2}
+          userId={userId}
+          deletedGamesRender={deletedGamesRender}
+          successAddMsg={successAddMsg}
+        />
+      )}
     </div>
   );
 }
--- client/src/Components/Pull-Gamelists/SteamList/SteamForm.js
+++ client/src/Components/Pull-Gamelists/SteamList/SteamForm.js
@@ -1,20 +1,18 @@
 import React from "react";
 
 function SteamForm({ submit, value, onChange }) {
-    return (
-      <form onSubmit={submit}>
+  return (
+    <div className="manualSteamLogin text-center">
+      <form  onSubmit={submit}>
         <p>
           If you know your Steam ID and your profile is public, please use your
           Steam ID otherwise please sign in with your Steam account.
         </p>
-        <input
-          type="text"
-          value={value}
-          onChange={onChange}
-        />
+        <input type="text" value={value} onChange={onChange} />
         <input type="submit" value="Get Steam games" />
       </form>
-    );
+    </div>
+  );
 }
 
 export default SteamForm;
