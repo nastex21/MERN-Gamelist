2e417953865dfa5a1300438e5158f78b32a8c66a
Tony <nasax2000@gmail.com>
2020-05-11 15:30:18 -0500

    Updated packages, changed Navbar to have a hamburger menu

--- client/src/Components/Navbar.js
+++ client/src/Components/Navbar.js
@@ -1,40 +1,55 @@
 import React from "react";
-import { Nav, Navbar, NavbarBrand } from 'react-bootstrap'; 
-import Logout from './AuthPages/Logout';
+import Navbar from "react-bootstrap/Navbar";
+import Nav from 'react-bootstrap/Nav'
+import Logout from "./AuthPages/Logout";
 
 function NavbarTop(props) {
-    console.log(props);
+  console.log(props);
 
-    const logoutHandler = e => {
-        Logout();
-    }   
+  const logoutHandler = (e) => {
+    Logout();
+  };
 
+  const clickHandler = () => {
+    props.setLocation(0);
+  };
 
-    const clickHandler = () => {
-        props.setLocation(0);
-    }
-     
-    console.log(props.guestUser);
-    return (
-        <>
-            <Navbar bg="dark" variant="dark" fixed="sticky">
-                <NavbarBrand href="/">Video Game Catalog</NavbarBrand>
-                <Nav className="ml-auto">
-                    {props.guestUser ? 
-                    <>
-                    <Nav.Link href="register" onClick={e => clickHandler}>Register</Nav.Link>
-                    <Navbar.Text >/</Navbar.Text>
-                    <Nav.Link href="login" onClick={e => clickHandler}>Log In</Nav.Link>
-                    <Navbar.Text >/</Navbar.Text>
-                    <Nav.Link href="dashboard">Continue as Guest</Nav.Link>
-                    </>
-                    : 
-                    <Nav.Link href="/" onClick={e=> logoutHandler(e)}>Log Out</Nav.Link>
-                    }
-                </Nav>
-            </Navbar>
-        </>
-    )
+  console.log(props.guestUser);
+  return (
+    <>
+      <Navbar
+        collapseOnSelect
+        expand="lg"
+        bg="dark"
+        variant="dark"
+        fixed="sticky"
+      >
+        <Navbar.Brand href="/">Video Game Catalog</Navbar.Brand>
+        {props.guestUser ? (
+          <>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+              <Nav className="ml-auto" aria-controls="responsive-navbar-nav">
+                <Nav.Link href="register" onClick={(e) => clickHandler}>
+                  Register
+                </Nav.Link>
+                <Nav.Link href="login" onClick={(e) => clickHandler}>
+                  Log In
+                </Nav.Link>
+                <Nav.Link href="dashboard">Continue as Guest</Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </>
+        ) : (
+          <Nav className="ml-auto">
+            <Nav.Link href="/" onClick={(e) => logoutHandler(e)}>
+              Log Out
+            </Nav.Link>
+          </Nav>
+        )}
+      </Navbar>
+    </>
+  );
 }
 
 export default NavbarTop;
