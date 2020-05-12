bbefed120d35986ff7fdfdf3501d2d7e5adb85f1
Tony <nasax2000@gmail.com>
2020-05-12 00:04:33 -0500

    Commit

--- client/src/Components/Dashboard.js
+++ client/src/Components/Dashboard.js
@@ -1,12 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import ManuallyAdded from "./Pull-Gamelists/ManualEntries/ManuallyAdded";
 import SteamForm from "./Pull-Gamelists/SteamList/SteamForm";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCaretDown, faCaretUp } from '@fortawesome/free-solid-svg-icons';
 import GenerateTable from "./GenerateTable/GenerateTable";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import Accordion from "react-bootstrap/Accordion";
+import Card from "react-bootstrap/Card";
 
-export default function Dashboard({
+function Dashboard({
   manualData,
   steamId,
   userId,
@@ -20,43 +24,71 @@ export default function Dashboard({
   deletedGamesRender,
   successAddMsg,
 }) {
-  console.log(successAddMsg);
+  const [open, setOpen] = useState("0");
+
+  const toggle = () => {
+    console.log("toggle");
+    console.log(open);
+    if (open == 0){
+      setOpen("1")
+    } else {
+      setOpen("0")
+    }
+  };
+
   return (
     <div className="buttonBox">
-      <div className="steamBox">
-        <Container fluid="true">
-          <Row className="align-items-center">
-            {!steamId ? (
-              <Col xs={5}>
-                <SteamForm
-                  value={value}
-                  onChange={handleChange}
-                  submit={handleSubmit}
-                />
-              </Col>
-            ) : null}
-            <Col md="auto" xs={1}>
-              <div className="vertLine"></div>
-            </Col>
-            {!steamId ? (
-              <Col xs={5}>
-                <div className="steamLogIn text-center">
-                  <a onClick={handleClick}>
-                    <img className="steamIMG" src="https://steamcommunity-a.akamaihd.net/public/images/signinthroughsteam/sits_01.png" />
-                  </a>
+      <div className="mobile-accordion">
+        <Accordion defaultActiveKey={open}>
+          <Card>
+            <Accordion.Toggle as={Card.Header} eventKey={open}  onClick={toggle} >
+              <FontAwesomeIcon icon={open == 0 ? faCaretUp : faCaretDown}/>
+            </Accordion.Toggle>
+            <Accordion.Collapse eventKey="0">
+              <Card.Body>
+                <div className="steamBox">
+                  <Container fluid="true">
+                    <Row className="align-items-center">
+                      {!steamId ? (
+                        <Col xs={5}>
+                          <SteamForm
+                            value={value}
+                            onChange={handleChange}
+                            submit={handleSubmit}
+                          />
+                        </Col>
+                      ) : null}
+                      <Col md="auto" xs={1}>
+                        <div className="vertLine"></div>
+                      </Col>
+                      {!steamId ? (
+                        <Col xs={5}>
+                          <div className="steamLogIn text-center">
+                            <a onClick={handleClick}>
+                              <img
+                                className="steamIMG"
+                                src="https://steamcommunity-a.akamaihd.net/public/images/signinthroughsteam/sits_01.png"
+                              />
+                            </a>
+                          </div>
+                        </Col>
+                      ) : null}
+                    </Row>
+                  </Container>
                 </div>
-              </Col>
-            ) : null}
-            {steamId ? (
-              <input
-                type="button"
-                value="Update Steam Games"
-                onClick={updateSteamGames}
-              />
-            ) : null}
-          </Row>
-        </Container>
+              </Card.Body>
+            </Accordion.Collapse>
+          </Card>
+        </Accordion>
       </div>
+
+      {steamId ? (
+        <input
+          type="button"
+          value="Update Steam Games"
+          onClick={updateSteamGames}
+        />
+      ) : null}
 
       <div className="dashboard">
         <div className="manualBox">
@@ -79,3 +111,5 @@ export default function Dashboard({
     </div>
   );
 }
+
+export default Dashboard;
